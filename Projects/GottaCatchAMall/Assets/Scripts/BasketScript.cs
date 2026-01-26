6f78785a6e0d91309596cb41792a33b959cd03e1
BenWoschi <benjaminw4101@gmail.com>
2026-01-26 00:53:46 -0500

    Continued following beginner Unity tutorial

---
+++ Projects/GottaCatchAMall/Assets/Scripts/BasketScript.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class BasketScript : MonoBehaviour {
+    public KeyCode left;
+    public KeyCode right;
+
+    public float basket_x, basket_y;
+    public float speed = 0.12f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(left)) {
+            // move left
+            basket_x -= speed;
+        }
+
+        if(Input.GetKeyDown(right)) {
+            // move right
+            basket_x += speed;
+        }
+
+        transform.position = new Vector3(basket_x, -4.0f, 0);
+    }
+}
